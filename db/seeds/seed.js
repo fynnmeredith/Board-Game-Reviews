6afb7d2238abccbfbae169ae1de7947279bf22b3
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-17 15:44:47 +0000

    Create tables and insert data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,82 @@
+const db = require("/Users/fynnmeredith/northcoders/Back-End/be-nc-games/db/connection")
+const format = require('pg-format');
+// const { formattedCategories } = require("../../utils");
+
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
-  // 2. insert data
-};
+  return db.query(`DROP TABLE IF EXISTS comments;`)
+  .then(() => {
+    return db.query(`DROP TABLE IF EXISTS reviews;`)
+  })
+  .then(() => {
+    return db.query(`DROP TABLE IF EXISTS users;`)
+  })
+  .then(() => {
+    return db.query(`DROP TABLE IF EXISTS categories`)
+  })
+  .then(() => {
+    return db.query(`CREATE TABLE categories (
+      slug VARCHAR(100) UNIQUE PRIMARY KEY,
+      description TEXT NOT NULL
+    );`)
+  })
+  .then(() => {
+    return db.query(`CREATE TABLE users (
+        username VARCHAR(50) UNIQUE PRIMARY KEY,
+        avatar_url TEXT NOT NULL,
+        name TEXT NOT NULL
+      );`)
+  })
+  .then(() => {
+    return db.query(`CREATE TABLE reviews (
+          review_id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          review_body TEXT NOT NULL,
+          designer TEXT NOT NULL,
+          review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+          votes INT DEFAULT 0,
+          category VARCHAR(100) NOT NULL REFERENCES categories(slug),
+          owner VARCHAR(50) NOT NULL REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+      );`)
+  })
+  .then(() => {
+        return db.query(`CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          author VARCHAR(50) REFERENCES users(username),
+          review_id INT REFERENCES reviews(review_id),
+          votes INT DEFAULT 0,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          body TEXT NOT NULL
+        )`)
+      })
+      // 2. insert data
+  .then(() => {
+    const formatCat = categoryData.map(cat => Object.values(cat));
+    const sql = format(`INSERT INTO categories (slug, description) VALUES %L RETURNING *;`, formatCat);
+    return db.query(sql)
+   .then(({ rows }) => {return rows})
+  })
+  .then(() => {
+    const formattedUsers = userData.map(user => Object.values(user));
+    const sql = format(`INSERT INTO users (username, avatar_url, name) VALUES %L RETURNING *;`, formattedUsers);
+    return db.query(sql)
+   .then(({ rows }) => { return rows })
+  })
+  .then(() => {
+    const formattedReviews = reviewData.map(review => Object.values(review));
+    const sql = format(`INSERT INTO reviews (title, designer, owner,  review_img_url, review_body, category, created_at, votes) VALUES %L RETURNING *;`, formattedReviews);
+    return db.query(sql)
+   .then(({ rows }) => {return rows})
+  })
+  .then(() => {
+    const formattedComments = commentData.map(comment => Object.values(comment));
+    const sql = format(`INSERT INTO comments (body, votes, author, review_id, created_at) VALUES %L RETURNING *;`, formattedComments);
+    return db.query(sql)
+   .then(({ rows }) => {return rows})
+  });
+}
 
 module.exports = seed;
