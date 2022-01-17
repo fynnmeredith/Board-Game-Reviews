008a5c3765520771a5b981fb962ab218a23bac19
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-17 17:22:12 +0000

    start of TDD for endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,38 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const seed = require('../db/seeds/seed.js');
+const app = require('../app')
+const request = require('supertest')
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('/api/categories', () => {
+    describe('GET', () => {
+      test('status 200, will return an array of categories', () => {
+        return request(app)
+          .get('/api/categories')
+          .expect(200)
+          .then((res) => {
+            res.body.categories.forEach((category) => {
+              expect(category).toEqual(
+                expect.objectContaining({
+                  slug: expect.any(String),
+                  description: expect.any(String),
+                })
+              );
+            });
+          });
+      });
+    });
+    describe('/api/invalid_url', () => {
+        test('Status: 404 and returns and error message', () => {
+            return request(app)
+                .get('/api/invalid_url')
+                .expect(404)
+                .then((res) => {
+                    expect(res.body.msg).toBe('Not Found');
+                });
+        });
+    });
+})
