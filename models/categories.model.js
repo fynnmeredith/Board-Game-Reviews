2769edeee77ddec3cfe798fd2ecc515f6f592dbc
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-17 17:21:08 +0000

    model for first endpoint

---
+++ models/categories.model.js
@@ -0,0 +1,9 @@
+const db = require('../db/connection.js')
+
+exports.selectCategories = () => {
+    return db.query('SELECT * FROM categories;')
+        .then(({ rows }) => {
+            return rows
+        })
+}
+
