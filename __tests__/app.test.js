44a53154c6d0676a1c564e2311e2ca07ce54e142
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-18 17:27:27 +0000

    add to tests for reviews endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,3 +36,37 @@ describe('/api/categories', () => {
         });
     });
 })
+
+describe('/api/reviews/:review_id', () => {
+  describe('GET', () => {
+    test('status 200, will return a review object matching the review id entered', () => {
+      return request(app)
+        .get('/api/reviews/2')
+        .expect(200)
+        .then((res) => {
+          expect(res.body.review).toEqual({
+            owner: expect.any(String),
+            title: expect.any(String),
+            review_id: 2,
+            review_body: expect.any(String),
+            designer: expect.any(String),
+            review_img_url: expect.any(String),
+            category: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            comment_count: expect.any(Number)
+          })
+        })
+      });
+    });
+  });
+  // describe('/api/invalid_url', () => {
+  //     test('Status: 404 and returns and error message', () => {
+  //         return request(app)
+  //             .get('/api/invalid_url')
+  //             .expect(404)
+  //             .then((res) => {
+  //                 expect(res.body.msg).toBe('Not Found');
+  //             });
+  //     });
+  // });
