aa44335655ef84a9cc8e4cfd21eecc0896eef904
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-18 17:26:19 +0000

    additions to app file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getCategories } = require('./controllers/categories.controller');
+const { getReviewById } = require('./controllers/reviews.controller');
 const app = express();
 
 app.use(express.json());
 
 app.get('/api/categories', getCategories)
 
-app.get('/api/reviews/:review_id')
+app.get('/api/reviews/:review_id', getReviewById)
 
 app.all('*', (req, res) => {
 	res.status(404).send({ msg: 'Not Found' });
