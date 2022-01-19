c019f75f58070f77f8247d2e6cc88c9418cc04f9
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-19 18:51:26 +0000

    add controller functions

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,10 +1,49 @@
-const { selectReviewById } = require("../models/reviews.model")
+const { selectReviewById, updateReviewVotes, selectOrderedReview, selectCommentsByReviewId } = require("../models/reviews.model")
 
 
 exports.getReviewById = (req, res, next) => {
     const review_id = req.params
     selectReviewById(review_id).then((review) => {
-        // console.log(review)
-        res.status(200).send({review})
+        if(review){
+         res.status(200).send({review})
+        } else {
+            return Promise.reject({status: 404, msg: "Not Found"})
+        }
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
+
+exports.patchReviewVotes = (req, res, next) => {
+    const { review_id } = req.params
+    const { inc_votes } = req.body
+    updateReviewVotes(inc_votes, review_id).then((review) => {
+        if (!review) {
+        return Promise.reject({status: 404, msg: "Not Found"})
+        } else {
+            res.status(200).send({ review })
+        }
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
+
+exports.getOrderedReview = (req, res, next) => {
+    const { sort_by, order, category } = req.query
+    selectOrderedReview(sort_by, order, category).then((reviews) => {
+        res.status(200).send({reviews})
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
+
+exports.getCommentsByReviewId = (req, res, next) => {
+    const reviewId = req.params
+    console.log(req.params)
+    selectCommentsByReviewId(reviewId).then((comments) => {
+        res.status(200).send({comments})
     })
 }
