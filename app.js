ef91d3f416716479747c1ae61f891fecdae77fec
fynnmeredith <fynnmeredith2@gmail.com>
2022-01-19 18:47:55 +0000

    add endpoints

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getCategories } = require('./controllers/categories.controller');
-const { getReviewById } = require('./controllers/reviews.controller');
+const { getReviewById, patchReviewVotes, getOrderedReview, getCommentsByReviewId } = require('./controllers/reviews.controller');
 const app = express();
 
 app.use(express.json());
@@ -9,10 +9,34 @@ app.get('/api/categories', getCategories)
 
 app.get('/api/reviews/:review_id', getReviewById)
 
+app.patch('/api/reviews/:review_id', patchReviewVotes)
+
+app.get('/api/reviews', getOrderedReview)
+
+app.get('/api/reviews/:review_id/comments', getCommentsByReviewId)
+
+
 app.all('*', (req, res) => {
 	res.status(404).send({ msg: 'Not Found' });
 });
 
+app.use((err, req, res, next) => {
+	if (err.code === '22P02' || err.code === '23502') {
+		res.status(400).send({ msg: 'Bad Request' });
+	} else {
+		next(err);
+	}
+});
+app.use((err, req, res, next) => {
+	if (err.status) {
+		res.status(err.status).send({ msg: err.msg });
+	} else {
+		next(err);
+	}
+});
+app.use((err, req, res, next) => {
+	res.status(500).send({ msg: 'Internal server error' });
+});
 
 
 
